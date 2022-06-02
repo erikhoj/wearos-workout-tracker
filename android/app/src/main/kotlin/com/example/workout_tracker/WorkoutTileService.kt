71fdaee869d09a1ee48c07b36cf515581fc5b7b9
Erik Hoej <erik@setsnail.com>
2022-06-03 00:01:00 +0200

    Started working on Firebase Auth for the tile

--- android/app/src/main/kotlin/com/example/workout_tracker/WorkoutTileService.kt
+++ android/app/src/main/kotlin/com/example/workout_tracker/WorkoutTileService.kt
@@ -1,8 +1,12 @@
 package com.example.workout_tracker
 
+import android.util.Log
+import android.widget.Button
+import androidx.wear.tiles.ActionBuilders
 import androidx.wear.tiles.LayoutElementBuilders
 import androidx.wear.tiles.LayoutElementBuilders.Layout
 import androidx.wear.tiles.LayoutElementBuilders.Text
+import androidx.wear.tiles.ModifiersBuilders
 import androidx.wear.tiles.RequestBuilders.ResourcesRequest
 import androidx.wear.tiles.RequestBuilders.TileRequest
 import androidx.wear.tiles.ResourceBuilders.Resources
@@ -10,6 +14,11 @@ import androidx.wear.tiles.TileBuilders.Tile
 import androidx.wear.tiles.TileService
 import androidx.wear.tiles.TimelineBuilders.Timeline
 import androidx.wear.tiles.TimelineBuilders.TimelineEntry
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.common.SignInButton
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.guava.future
@@ -29,14 +38,48 @@ class WorkoutTileService : TileService() {
                         Layout.Builder().setRoot(
                             Text.Builder().setText("Hello World").setFontStyle(
                                 LayoutElementBuilders.FontStyle.Builder().build()
+                            ).setModifiers(
+                                ModifiersBuilders.Modifiers.Builder()
+                                    .setClickable(
+                                        ModifiersBuilders.Clickable.Builder()
+                                            .setId("TEST_ID")
+                                            .setOnClick()
+                                            .build()
+                                    ).build()
                             ).build()
                         ).build()
                     ).build()
                 ).build()
             ).build()
     }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val button = 
+    }
         
     override fun onResourcesRequest(requestParams: ResourcesRequest) = serviceScope.future {
+        val auth = Firebase.auth
+        Log.d("Test", "Test: Current User: " + auth.currentUser?.uid)
+
+        val signInRequest = BeginSignInRequest.Builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(getString(R.string.default_web_client_id))
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            .build()
+
+
+        val db = Firebase.firestore
+        db.collection("test").add(hashMapOf(
+            "first" to "Test",
+        ))
+
+        Log.d("Test", "Test Log")
+
         Resources.Builder()
             .setVersion(RESOURCES_VERSION)
             .build()
